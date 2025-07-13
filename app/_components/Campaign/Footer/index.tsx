0dfb26d3657bff8fad31a72a162996ad8706ee6a
shimojit <0liberty4life@gmail.com>
2025-07-13 22:10:00 +0900

    2025/07/13

---
+++ app/_components/Campaign/Footer/index.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+import styles from './index.module.css';
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <nav className={styles.nav}>
+        <ul className={styles.items}>
+          <li className={styles.item}>
+            <Link href="/news">記事一覧</Link>
+          </li>
+          <li className={styles.item}>
+            <Link href="/members">自己紹介（今後作成予定）</Link>
+          </li>
+          <li className={styles.item}>
+            <Link href="/contact">お問い合わせ（今後作成予定）</Link>
+          </li>
+        </ul>
+      </nav>
+      <p className={styles.cr}>© shimojit. ALL Rights Reserved 2025</p>
+    </footer>
+  );
+}
---
+++ app/_components/Campaign/Menu/index.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
+import cx from "classnames";
+import styles from "./index.module.css";
+
+export default function Menu() {
+  const [isOpen, setOpen] = useState<boolean>(false);
+  const open = () => setOpen(true);
+  const close = () => setOpen(false);
+  return (
+    <div>
+      <nav className={cx(styles.nav, isOpen && styles.open)}>
+        <ul className={styles.items}>
+          <li>
+            <Link href="/news">記事一覧</Link>
+          </li>
+          <li>
+            <Link href="/members">自己紹介（今後作成予定）</Link>
+          </li>
+          <li>
+            <Link href="/contact">お問い合わせ（今後作成予定）</Link>
+          </li>
+          <li>
+            <Link href="/campaign">食洗器ページ（テスト用ページ）</Link>
+          </li>
+        </ul>
+        <button className={cx(styles.button, styles.close)} onClick={close}>
+          <Image
+            src="/close.svg"
+            alt="閉じる"
+            width={24}
+            height={24}
+            priority
+          />
+        </button>
+      </nav>
+      <button className={styles.button} onClick={open}>
+        <Image src="/menu.svg" alt="メニュー" width={24} height={24} />
+      </button>
+    </div>
+  );
+}
